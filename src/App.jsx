a213d24ff2c1bdf96ffb0e049ce8545e3a82e072
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-08-20 13:30:57 +0430

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    
+    useEffect(() => {
+    fetch("http://localhost:3000/meals")
+      .then((res) => res.json())
+      .then(console.log);
+  }, []);
+  
+    return (
+        <>
+            <h1>You got this 💪</h1>
+            <p>Stuck? Not sure how to proceed?</p>
+            <p>Don't worry - we've all been there. Let's build it together!</p>
+        </>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
