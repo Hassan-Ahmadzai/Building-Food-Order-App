224356e76812a083a4f4a7c02f109f84af2de111
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-08-21 10:28:10 +0430

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Meals from "./components/Meals";
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     return (
         <>
             <Header />
+            <Meals />
         </>
     );
 };
