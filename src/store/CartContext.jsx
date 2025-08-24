f68bbd1981706518cbe83cec4bb4d678aad6c835
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-08-24 14:40:44 +0430

    First Initial

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,3 +1,4 @@
+import js from "@eslint/js";
 import { createContext, useReducer } from "react";
 
 const CartContext = createContext({
@@ -34,6 +35,24 @@ function cartReducer(state, action) {
 
     if (action.type === 'REMOVE_ITEM') {
         // ... remove an item from the state
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+        );
+        const existingCartItem = state.items[existingCartItemIndex];
+        const updatedItems = [...state.items];
+
+        if (existingCartItem.quantity === 1) {
+            updatedItems.splice(existingCartItemIndex, 1);
+        } else {
+            const updatedItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity - 1,
+            };
+
+            updatedItems[existingCartItemIndex] = updatedItem;
+        };
+
+        return { ...state, items: updatedItems };
     };
 
     return state;
@@ -41,10 +60,23 @@ function cartReducer(state, action) {
  
 
 export default function CartContextProvider({ children }) {
-    useReducer(cartReducer, { items: [] });
+    const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+    function addItem(item) {
+        dispatchCartAction({ type: 'ADD_ITEM', item });
+    };
+    function removeItem(id) {
+        dispatchCartAction({ type: 'REMOVE_ITEM', id });
+    };
+
+    const cartContext = {
+        items: cart.items,
+        addItem: addItem,
+        removeItem: removeItem,
+    };
 
     return (
-        <CartContext.Provider>
+        <CartContext.Provider value={cartContext}>
             {children}
         </CartContext.Provider>
     );
