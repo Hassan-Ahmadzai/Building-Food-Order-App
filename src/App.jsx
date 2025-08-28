c8c7d3c120ff04d68eb956d190ca73c3ca7f7cf8
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-08-28 11:29:52 +0430

    files deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import Header from "./components/Header";
-import Meals from "./components/Meals";
-import CartContextProvider from "./store/CartContext";
-import UserProgressContextProvider from "./store/UserProgressContext";
+
 
     
 function App() {
     return (
-        <UserProgressContextProvider>
-            <CartContextProvider>
-                <Header />
-                <Meals />
-            </CartContextProvider>
-        </UserProgressContextProvider>
+        <>
+        </>
     );
 };
 
