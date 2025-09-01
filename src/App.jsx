ec68c1d5afe5713cc02e1aee4ff1ac182249e789
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-01 13:08:21 +0430

    erased all unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
-import Meals from "./components/Meals";
-import { CartContextProvider } from "./store/CartContext";
-import Cart from "./components/Cart";
-import { UserProgressContextProvider } from "./store/UserProgressContext";
 
         
 function App() {
     return (
-        <UserProgressContextProvider>
-            <CartContextProvider>
-                <Header />
-                <Meals />
-                <Cart />
-            </CartContextProvider>
-        </UserProgressContextProvider>
+        <>
+        </>
     );
 };
 
