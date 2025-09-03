c29a6d01176cdb606a52919745ba2e0060801fef
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-03 14:30:21 +0430

    Function created

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,9 @@
+import { createPortal } from "react-dom";
+
+
+export default function Modal() {
+    return createPortal(
+        <dialog></dialog>,
+        document.getElementById('modal')
+    );
+};
