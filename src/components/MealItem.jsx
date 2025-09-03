68863711571697ba0bb88a979252fdaed44c7168
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-03 07:42:17 +0430

    Initial Commit

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,8 +1,16 @@
+import { useContext } from "react";
 import { currencyFormatter } from "../util/Formatter";
 import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 
 export default function MealItem({ meal }) {
+    const cartCtx = useContext(CartContext);
+
+    function handleAddMealToCart() {
+        cartCtx.addItem(meal);
+    };
+
     return (
         <li className="meal-item">
             <article>
@@ -15,7 +23,7 @@ export default function MealItem({ meal }) {
                 </div>
 
                 <p className="meal-item-actions">
-                    <Button>Add to Cart </Button>
+                    <Button onClick={handleAddMealToCart}>Add to Cart </Button>
                 </p>
             </article>
         </li>
