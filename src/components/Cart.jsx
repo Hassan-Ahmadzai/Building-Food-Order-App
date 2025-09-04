f61089d3461af82b2347fb0eda49678468f9da79
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-04 11:02:47 +0430

    function created

---
+++ src/components/Cart.jsx
@@ -0,0 +1,13 @@
+import Modal from "./UI/Modal";
+
+
+export default function Cart() {
+    return (
+        <Modal>
+            <h2>Your Cart</h2>
+            <ul>
+
+            </ul>
+        </Modal>
+    );
+};
