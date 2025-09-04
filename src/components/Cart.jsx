b5f61de1fb8c0c426b23ab443e79e6edeba9472c
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-04 11:35:19 +0430

    Modal created

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,19 +2,25 @@ import { createContext, useContext } from "react";
 import Modal from "./UI/Modal";
 import CartContext from '../store/CartContext';
 import Button from './UI/Button';
-import { currentFormatter } from '../util/Formatter';
+import UserProgressContext from "../store/UserProgressContext";
+import { currencyFormatter } from "../util/Formatter";
 
 
 export default function Cart() {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
 
     const cartTotal = cartCtx.items.reduce(
         (totalPrice, item) => totalPrice + item.quantity * item.price,
         0
     );
 
+    function handleCloseCart() {
+        userProgressCtx.hideCart();
+    };
+
     return (
-        <Modal className='cart'>
+        <Modal className='cart' open={userProgressCtx.progress === 'cart'}>
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map((item) => (
@@ -24,9 +30,9 @@ export default function Cart() {
                 ))}
             </ul>
 
-            <p className="cart-total">{currentFormatter.format(cartTotal)}</p>
+            <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
-                <Button>Close</Button>
+                <Button onClick={handleCloseCart}>Close</Button>
                 <Button>Go to Checkout</Button>
             </p>
         </Modal>
--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -5,11 +5,15 @@ import { createPortal } from "react-dom";
 export default function Modal({ children, open, className }) {
     const dialog = useRef();
 
-    // useEffect(() => {
-    //     if (open) {
-    //         dialog.current.showModal();
-    //     };
-    // }, [open]);
+    useEffect(() => {
+        const modal = dialog.current;
+
+        if (open) {
+            modal.showModal();
+        };
+
+        return () => modal.close();
+    }, [open]);
 
     return createPortal(
         <dialog ref={dialog} className={`modal ${className}`}>
