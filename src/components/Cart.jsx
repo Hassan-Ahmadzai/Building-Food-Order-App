7704f4dbd5b9d2e6d32a423c33f3fa5b3951ca17
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-04 11:10:36 +0430

    component updated

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,34 @@
+import { createContext, useContext } from "react";
 import Modal from "./UI/Modal";
+import CartContext from '../store/CartContext';
+import Button from './UI/Button';
+import { currentFormatter } from '../util/Formatter';
 
 
 export default function Cart() {
+    const cartCtx = useContext(CartContext);
+
+    const cartTotal = cartCtx.items.reduce(
+        (totalPrice, item) => totalPrice + item.quantity * item.price,
+        0
+    );
+
     return (
-        <Modal>
+        <Modal className='cart'>
             <h2>Your Cart</h2>
             <ul>
-
+                {cartCtx.items.map((item) => (
+                    <li key={item.id}>
+                        {item.name} - {item.quantity}
+                    </li>
+                ))}
             </ul>
+
+            <p className="cart-total">{currentFormatter.format(cartTotal)}</p>
+            <p className="modal-actions">
+                <Button>Close</Button>
+                <Button>Go to Checkout</Button>
+            </p>
         </Modal>
     );
 };
