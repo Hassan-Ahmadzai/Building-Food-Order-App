a4ce4a721978fe94f7e6ed56f2f77c007daae01c
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-06 14:33:21 +0430

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
-import Meals from "./components/Meals";
-import { CartContextProvider } from "./store/CartContext";
-import { UserProgressContextProvider } from "./store/UserProgressContext";
-import Cart from "./components/Cart";
-import Checkout from "./components/Checkout";
 
         
 function App() {
     return (
-        <UserProgressContextProvider>
-            <CartContextProvider>
-                <Header />
-                <Meals />
-                <Cart />
-                <Checkout />
-            </CartContextProvider>
-        </UserProgressContextProvider>
+        <>
+            <Header />
+        </>
     );
 };
 
