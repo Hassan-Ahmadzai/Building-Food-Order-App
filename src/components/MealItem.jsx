7311a8ce1a0d829e615c97811e6d85b6f423a175
Shakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-07 10:54:53 -0700

    Design updated

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,4 +1,4 @@
-
+import { currencyFormatter } from "../util/formatting";
 
 export default function MealItem({ meal }) {
     return (
@@ -8,7 +8,7 @@ export default function MealItem({ meal }) {
 
                 <div>
                     <h3>{meal.name}</h3>
-                    <p className="meal-item-price">{meal.price}</p>
+                    <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
                     <p className="meal-item-description">{meal.description}</p>
                 </div>
 
