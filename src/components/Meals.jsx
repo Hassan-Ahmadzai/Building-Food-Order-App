e4cb6f61e7fc2428c135c50006a89d0cf81b3d08
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-08 15:24:21 +0430

    updates the structure of files

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -20,6 +20,21 @@ export default function Meals() {
         fetchMeals();
     }, []);
 
+     useEffect(() => {
+        async function fetchMeals() {
+            const response = await fetch('http://localhost:3000/meals');
+
+            if (!response.ok) {
+                // ...
+            };
+
+            const meals = await response.json();
+            setLoadedMeals(meals);
+        };
+
+        fetchMeals();
+    }, []);
+
     return (
         <ul id="meals">
             {loadedMeals.map((meal) => (
