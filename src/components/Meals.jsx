01b2f943d508901cfafe70110a15248fc4e602be
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-09 11:43:16 +0430

    Initial Commit

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -20,21 +20,6 @@ export default function Meals() {
         fetchMeals();
     }, []);
 
-     useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
-
-            if (!response.ok) {
-                // ...
-            };
-
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        };
-
-        fetchMeals();
-    }, []);
-
     return (
         <ul id="meals">
             {loadedMeals.map((meal) => (
