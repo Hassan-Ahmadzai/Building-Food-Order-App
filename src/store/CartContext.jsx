b77bd6fa0debfe2e22461bd777ede53c3ee84daf
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-09 11:48:01 +0430

    function updated

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -31,7 +31,7 @@ function cartReducer(state, action) {
 
     if (action.type === 'REMOVE_ITEM') {
         const existingCartItemIndex = state.items.findIndex(
-            (item) => item.id === action.item.id
+            (item) => item.id === action.id
         );
 
         const existingCartItem = state.items[existingCartItemIndex];
@@ -58,9 +58,22 @@ function cartReducer(state, action) {
 export function CartContextProvider({ children }) {
     const [ cart, dispatchCartAction ] = useReducer(cartReducer, { items: [] });
 
+    function addItem(item) {
+        dispatchCartAction({ type: 'ADD_ITEM', item: item });
+    };
+
+    function removeItem(id) {
+        dispatchCartAction({ type: 'REMOVE_ITEM', id });
+    };
+
+    const cartContext = {
+        items: cart.items,
+        addItem,
+        removeItem,
+    };
 
     return (
-        <CartContext.Provider>
+        <CartContext.Provider value={cartContext}>
             {children}
         </CartContext.Provider>
     );
